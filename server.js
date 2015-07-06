69d5ee438caef173c1c84fbc5d2f3a6850b78f21
Daniel Gibson <gib.dan97@gmail.com>
2015-07-06 11:57:29 +1000

    Added further stuff with Jade

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app=express();
 
-app.get('/', function(req, res) {
-    res.send("Further testing stuff");
+app.set('views', '/Users/dgibson/dev/express_test/node_modules/views');
+app.set('view engine', 'jade');
+
+app.get('/test', function(req, res) {
+    res.render('index', { title: 'New Tab', message: Math.random(), message2: 'This is a paragraph.'});
 });
 
-app.get('/random', function(req, res) {
+app.get('/diff', function(req, res) {
+    res.send("This is a dead end");
+});
+
+app.get('/', function(req, res) {
     res.status(200).json({ "number": Math.random() });
 });
 

11452344659226915e2b89db11d7ad55609614d5
Daniel Gibson <gib.dan97@gmail.com>
2015-07-06 12:37:17 +1000

    Added CSS - red color

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ app.set('views', __dirname + '/app/views');
 app.set('view engine', 'jade');
 app.use('/', require('./app/routes')(router));
 
-
 app.listen(4000, function() {
     console.log("Currently running on port 4000!");
 });

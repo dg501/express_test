34be3966f1e19240be1a3eb1e849f8273c58ef96
Daniel Gibson <gib.dan97@gmail.com>
2015-07-06 12:08:59 +1000

    removed hardcoded path; removed router; added jade file to source control

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var app=express();
 
-app.set('views', '/Users/dgibson/dev/express_test/node_modules/views');
+app.set('views', __dirname + '/app/views');
 app.set('view engine', 'jade');
 
-app.get('/test', function(req, res) {
-    res.render('index', { title: 'New Tab', message: Math.random(), message2: 'This is a paragraph.'});
+app.get('/test', function(request, response) {
+    response.render('index', { message: Math.random() });
 });
 
-app.get('/diff', function(req, res) {
-    res.send("This is a dead end");
+app.get('/diff', function(request, response) {
+    response.send("This is a dead end");
 });
 
-app.get('/', function(req, res) {
-    res.status(200).json({ "number": Math.random() });
+app.get('/', function(request, response) {
+    response.status(200).json({ "number": Math.random() });
 });
 
 var server=app.listen(4000, function() {

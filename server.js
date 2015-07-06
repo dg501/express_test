51c371b1b000a93de8687ac341741f507d96b56e
Daniel Gibson <gib.dan97@gmail.com>
2015-07-06 12:19:35 +1000

    Fixed broken code

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.set('view engine', 'jade');
 
 app.get('/test', require('./app/controllers/human-readable-random'));
 app.get('/diff', require ('./app/controllers/automated-message'));
-app.get('/', require ('./app/controllers/unused-page')
+app.get('/', require ('./app/controllers/unused-page'));
 
 var server=app.listen(4000, function() {
     console.log("Currently running on port 4000!");

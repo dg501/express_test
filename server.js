026b17e743a28e28981b50b6218150b4fa19b44d
Daniel Gibson <gib.dan97@gmail.com>
2015-07-06 12:21:07 +1000

    fixing whitespace - so important ;)

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var app=express();
+var app = express();
 
 app.set('views', __dirname + '/app/views');
 app.set('view engine', 'jade');
@@ -8,6 +8,6 @@ app.get('/test', require('./app/controllers/human-readable-random'));
 app.get('/diff', require ('./app/controllers/automated-message'));
 app.get('/', require ('./app/controllers/unused-page'));
 
-var server=app.listen(4000, function() {
+app.listen(4000, function() {
     console.log("Currently running on port 4000!");
 });

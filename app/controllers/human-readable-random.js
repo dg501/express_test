7fcde411ab8ee050db8b91a906a3edf461b7f41f
Daniel Gibson <gib.dan97@gmail.com>
2015-07-08 14:53:52 +1000

    Fixed further problems, and changed a few things

--- app/controllers/human-readable-random.js
+++ app/controllers/human-readable-random.js
@@ -1,7 +1,12 @@
 var RandomNumber = require('../models/random-number');
 
 module.exports = function(request, response) {
-    RandomNumber.findLatest().then(function(result){
+    RandomNumber.findLatest(function(error, result) {
+
+        if (error) {
+            console.log("Error! " + error);
+        }
+
         response.render('index', { message: result[0].value});
     });
 };
